752049c530851df97c899903ef405ce12b12ffab
Azeem Gulwani <azeemgulwani@usf.edu>
2025-09-17 19:50:08 -0400

    Created List of Products

--- cc_4b.js
+++ cc_4b.js
@@ -1,2 +1,9 @@
 // Coding Challenge 4b
 
+const products = [
+    {SKU : "SKU-001", name : "Wireless Mouse", category : "electronics", price: 29.99, inventory: 12},
+    {SKU : "SKU-002", name : "Tee", category : "apparel", price: 18.50, inventory: 15},
+    {SKU : "SKU-003", name : "Oats", category : "groceries", price: 5.99, inventory: 10},
+    {SKU : "SKU-004", name : "Towela", category : "household", price: 12.99, inventory: 7},
+    {SKU : "SKU-005", name : "Charger", category : "electronics", price: 23.99, inventory: 9},
+]
