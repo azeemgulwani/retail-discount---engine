259bddd54ed6b3735f832f90b5889d4feed4e1b7
Azeem Gulwani <azeemgulwani@usf.edu>
2025-09-17 20:03:31 -0400

    Applied base discount using switch

--- cc_4b.js
+++ cc_4b.js
@@ -6,4 +6,28 @@ const products = [
     {SKU : "SKU-003", name : "Oats", category : "groceries", price: 5.99, inventory: 10},
     {SKU : "SKU-004", name : "Towela", category : "household", price: 12.99, inventory: 7},
     {SKU : "SKU-005", name : "Charger", category : "electronics", price: 23.99, inventory: 9},
-]
+];
+
+// Step 3:
+for (let product of products) {
+    let discount = 0;
+    switch (product.category) {
+        case "electronics":
+            discount = 0.2;
+            break;
+
+        case "apparel":
+            discount = 0.15;
+            break;
+        
+        case "groceries": 
+        case "household":
+            discount = 0.1;
+            break;
+    
+        default:
+            discount = 0;
+            break;
+    }
+    
+}
